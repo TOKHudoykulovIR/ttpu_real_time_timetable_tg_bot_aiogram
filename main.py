a2e8ab999aa12b89703c810e00f57f41f7834b45
TOKHudoykulovIR <tokhirjonbek@gmail.com>
2022-07-06 17:13:31 +0500

    add mem stor

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from aiogram import Bot, Dispatcher, executor
 import os
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 from sqlite import sql_start
 
 load_dotenv()
 
+storage = MemoryStorage()
+
 API_TOKEN = os.getenv("API_TOKEN")
 api_token = API_TOKEN
 bot = Bot(token=api_token)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
 
 async def on_startup(dp):
