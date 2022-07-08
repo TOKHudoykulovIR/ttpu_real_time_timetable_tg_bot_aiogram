c666f0d341866446106731e6151aea8a3b22d415
TOKHudoykulovIR <tokhirjonbek@gmail.com>
2022-07-08 15:09:11 +0500

    commit 26

--- handlers.py
+++ handlers.py
@@ -311,7 +311,7 @@ async def py_catalog(call: CallbackQuery):
 async def contacts(call: CallbackQuery):
     await call.message.answer('choose faculty', reply_markup=faculty_keyboard)
     await call.message.edit_reply_markup(reply_markup=None)
-    await FSMMenu.first_lvl.set()
+    # await FSMMenu.first_lvl.set()
 
 
 @dp.callback_query_handler(cd_course.filter(course=["second_lvl"]), state="*")
