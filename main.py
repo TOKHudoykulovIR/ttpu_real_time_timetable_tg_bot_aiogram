171a3c630d617b9b00fb108e8da098b6daa96f39
TOKHudoykulovIR <tokhirjonbek@gmail.com>
2022-08-16 17:27:19 +0500

    some changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from aiogram.types import BotCommand
 
 async def set_commands(bot: Bot):
     commands = [
-        BotCommand(command="/start", description="Get Menu"),
+        BotCommand(command="/start", description="Open Menu"),
         BotCommand(command="/help", description="Get Instructions"),
     ]
     await bot.set_my_commands(commands)
